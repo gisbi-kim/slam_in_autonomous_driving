5d7dccedb294d371533081eddff89c4cf44b506f
Giseop Kim <gisbi.kim@gmail.com>
2023-06-04 13:51:04 +0900

    gskim: ch2 wip

--- src/ch2/motion.cc
+++ src/ch2/motion.cc
@@ -1,6 +1,10 @@
 //
 // Created by xiang on 22-12-29.
+// Refactored by Giseop Kim on 23-06-05
 //
+
+#include <chrono>
+#include <thread>
 
 #include <gflags/gflags.h>
 #include <glog/logging.h>
@@ -9,12 +13,12 @@
 #include "common/math_utils.h"
 #include "tools/ui/pangolin_window.h"
 
-/// 本节程序演示一个正在作圆周运动的车辆
-/// 车辆的角速度与线速度可以在flags中设置
+/// This section demonstrates a vehicle performing circular motion.
+/// The angular velocity and linear velocity of the vehicle can be set in the flags.
 
-DEFINE_double(angular_velocity, 10.0, "角速度（角度）制");
-DEFINE_double(linear_velocity, 5.0, "车辆前进线速度 m/s");
-DEFINE_bool(use_quaternion, false, "是否使用四元数计算");
+DEFINE_double(angular_velocity, 10.0, "Angular velocity (in degrees)");
+DEFINE_double(linear_velocity, 5.0, "Linear velocity of the vehicle in m/s");
+DEFINE_bool(use_quaternion, false, "Whether to use quaternion calculation");
 
 int main(int argc, char** argv) {
     google::InitGoogleLogging(argv[0]);
@@ -22,26 +26,27 @@ int main(int argc, char** argv) {
     FLAGS_colorlogtostderr = true;
     google::ParseCommandLineFlags(&argc, &argv, true);
 
-    /// 可视化
+    /// Visualization
     sad::ui::PangolinWindow ui;
-    if (ui.Init() == false) {
+    if (!ui.Init()) {
         return -1;
     }
 
-    double angular_velocity_rad = FLAGS_angular_velocity * sad::math::kDEG2RAD;  // 弧度制角速度
-    SE3 pose;                                                                    // TWB表示的位姿
-    Vec3d omega(0, 0, angular_velocity_rad);                                     // 角速度矢量
-    Vec3d v_body(FLAGS_linear_velocity, 0, 0);                                   // 本体系速度
-    const double dt = 0.05;                                                      // 每次更新的时间
+    const double angular_velocity_rad = FLAGS_angular_velocity * sad::math::kDEG2RAD;  // Angular velocity in radians
+    const Vec3d omega(0, 0, angular_velocity_rad);                                     // Angular velocity vector
+    const Vec3d v_body(FLAGS_linear_velocity, 0, 0);                                   // Body-frame velocity
+    const double dt = 0.5;                                                             // Time for each update
 
-    while (ui.ShouldQuit() == false) {
-        // 更新自身位置
+    SE3 pose;  // Pose represented by TWB (B: body, W: world)
+    while (!ui.ShouldQuit()) {
+        // Update ego position global
         Vec3d v_world = pose.so3() * v_body;
         pose.translation() += v_world * dt;
 
-        // 更新自身旋转
+        // Update ego orientation global
         if (FLAGS_use_quaternion) {
-            Quatd q = pose.unit_quaternion() * Quatd(1, 0.5 * omega[0] * dt, 0.5 * omega[1] * dt, 0.5 * omega[2] * dt);
+            auto dq = Quatd(1, 0.5 * omega[0] * dt, 0.5 * omega[1] * dt, 0.5 * omega[2] * dt);
+            auto q = pose.unit_quaternion() * dq;
             q.normalize();
             pose.so3() = SO3(q);
         } else {
@@ -51,7 +56,8 @@ int main(int argc, char** argv) {
         LOG(INFO) << "pose: " << pose.translation().transpose();
         ui.UpdateNavState(sad::NavStated(0, pose, v_world));
 
-        usleep(dt * 1e6);
+        const std::chrono::microseconds sleepTime(static_cast<long long>(dt * 1e6));
+        std::this_thread::sleep_for(sleepTime);
     }
 
     ui.Quit();
